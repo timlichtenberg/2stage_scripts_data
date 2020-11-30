90e678ac64c069973f0f7834c2414428ba869dfc
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2020-11-30 15:06:05 +0000

    Fig 1 update

--- fig_1.py
+++ fig_1.py
@@ -19,23 +19,21 @@ gs = fig.add_gridspec(nrows=15, ncols=13, wspace=0.15, hspace=0.15, left=0.055, 
 sns.set(style="ticks")
 ax1 = fig.add_subplot(gs[0:15, 0:15])
 
-ax1.set_xlim([0.5,25.])
+ax1.set_xlim([0.5,21.])
 ax1.set_ylim([1e+5,5e+6])
 ax1.set_xscale('log')
 ax1.set_yscale('log')
 ax1.set_xlabel(r'Orbital distance, $r\ (\rm{au})$', fontsize=fsize+2)
 ax1.set_ylabel(r'Time, $t\ (\rm{Myr})$', fontsize=fsize+2)
-ax1.set_xticks( [0.5, 1, 2, 3, 5, 10, 20] )
-ax1.set_xticklabels( ["0.5", "1", "2", "3", "5", "10", "20"], fontsize=fsize-2 )
+ax1.set_xticks( [0.5, 1, 2, 3, 5, 7, 10, 14, 20] )
+ax1.set_xticklabels( ["0.5", "1", "2", "3", "5", "7", "10", "14", "20"], fontsize=fsize-2 )
 ax1.set_yticks( [1e+5, 2e+5, 4e+5, 7e+5, 1e+6, 2e+6, 3e+6, 4e+6, 5e+6])
 ax1.set_yticklabels(["0.1", "0.2", "0.4", "0.7", "1.0", "2.0", "3.0", "4.0", "5.0"], fontsize=fsize-2)
 
-# cgs units
-no_colors = 255
-v = np.linspace(-33, -26, no_colors, endpoint=True)
-
-# Axis unit
-v_legend = np.linspace(-33, -26, 8, endpoint=True)
+# cgs color axis units
+no_colors       = 255
+v               = np.linspace(-33, -26, no_colors, endpoint=True)
+v_legend        = np.linspace(-33, -26, 8, endpoint=True)
 
 # Mearth au-2 Myr-1 units
 v_nat           = [ (10**(i))*((au**2.)*(1e+6*year))/Mea for i in v_legend ]
@@ -44,7 +42,7 @@ v_nat_mag_str   = [ str(i) for i in v_nat_mag ]
 print("Natural units, order of magnitude:", v_nat_mag)
 
 # Snowline
-ax1.plot(q.rsnow[26:]/au,q.time[26:]/year,color="black",linestyle='-', lw=2.5, zorder=1)
+ax1.plot(q.rsnow[26:]/au,q.time[26:]/year, color="#530971", ls='-', lw=2.5, zorder=1)
 
 for i in range(0, 3):
     cont_resI = ax1.contourf(q.r/au, q.time/year, np.log(q.dSigmadt_resI), v, cmap='Reds', interpolation='nearest',zorder=2)
@@ -52,22 +50,25 @@ for i in range(0, 3):
 
 # Colorbar legends
 xloc_colorlegend = -0.03
-yloc_colorlegend = +0.10
+yloc_colorlegend = +0.12
 
 # Res I
-cax = inset_axes(ax1, width='30%', height='2%', loc='lower right', bbox_to_anchor=(xloc_colorlegend, yloc_colorlegend, 1, 1), bbox_transform=ax1.transAxes, borderpad=0)
+cax = inset_axes(ax1, width='35%', height='2%', loc='lower right', bbox_to_anchor=(xloc_colorlegend, yloc_colorlegend, 1, 1), bbox_transform=ax1.transAxes, borderpad=0)
 clb = plt.colorbar(cont_resI, cax=cax, ticks=v_legend, orientation="horizontal")
 clb.ax.tick_params(labelsize=fsize-7, labelrotation=45)
-# Res II
-cbaxes2 = inset_axes(ax1, width="30%", height="2%", loc='lower right', bbox_to_anchor=(xloc_colorlegend, yloc_colorlegend+0.03, 1, 1), bbox_transform=ax1.transAxes, borderpad=0.0)
-clb2 = plt.colorbar(cont_resII, cax=cbaxes2, ticks=v_legend, orientation="horizontal")
-clb2.ax.tick_params(labelsize=12, labelrotation=45)
-clb2.ax.set_xticklabels(v_nat_mag_str)
-cbaxes2.xaxis.set_ticks_position('top')
 
-clb2.ax.text(-0.5, -0.5, 'Planetesimal formation\nrate, '+r'$\mathrm{d}\Sigma_{\rm{plts}}/\mathrm{d}t$', fontsize=fsize-3, rotation=0, va='center', ha="center") 
-clb2.ax.text(0.55, 4.6, r'log $M_\mathrm{Earth}\ \mathrm{au}^{-2}\ \mathrm{Myr}^{-1}$', fontsize=fsize-5, rotation=0, va='center', ha="center") 
-clb.ax.text(0.55, -3.4, r'log $\mathrm{g}\ \mathrm{cm}^{-2}\ \mathrm{s}^{-1}$', fontsize=fsize-5, rotation=0, va='center', ha="center") 
+# Res II
+cbaxes2 = inset_axes(ax1, width="35%", height="2%", loc='lower right', bbox_to_anchor=(xloc_colorlegend, yloc_colorlegend+0.0205, 1, 1), bbox_transform=ax1.transAxes, borderpad=0.0)
+clb2 = plt.colorbar(cont_resII, cax=cbaxes2, ticks=v_legend, orientation="horizontal")
+clb2.ax.tick_params(labelsize=fsize-7, labelrotation=45, width=0, length=43)
+clb2.ax.set_xticklabels(v_nat_mag_str)
+# cbaxes2.xaxis.set_ticks_position('top')
+
+clb2.ax.text(+0.5, +2.5, 'Planetesimal formation rate, '+r'$\mathrm{d}\Sigma_{\rm{plts}}/\mathrm{d}t$', fontsize=fsize-4, rotation=0, va='center', ha="center") 
+
+clb.ax.text(-0.1, -2.0, r'log $\mathrm{g}\ \mathrm{cm}^{-2}\ \mathrm{s}^{-1}$', fontsize=fsize-7, rotation=0, va='center', ha="right") 
+
+clb2.ax.text(-0.1, -5.0, r'log $\mathrm{M}_\mathrm{Earth}\ \mathrm{au}^{-2}\ \mathrm{Myr}^{-1}$', fontsize=fsize-7, rotation=0, va='center', ha="right") 
 
 ############ MIGRATION
 X           = [ float(x) for x in list(df_gapmap.columns.values)[1:]]
@@ -128,9 +129,9 @@ for idx, track_names in enumerate([ ('350000.0yr0.1Mearth3.0au', '350000.0yr0.01
     track1_name, track2_name = track_names
 
     if idx == 0 or idx == 1:
-        mig_col         = qred_dark
-        mig_col_dot     = qred_dark
-        mig_col_cone    = qred_dark
+        mig_col         = qgray_dark
+        mig_col_dot     = qgray_dark
+        mig_col_cone    = qgray_dark
     if idx == 2:
         mig_col         = qgreen
         mig_col_dot     = qgreen_dark
@@ -154,29 +155,29 @@ for idx, track_names in enumerate([ ('350000.0yr0.1Mearth3.0au', '350000.0yr0.01
 
     ax1.fill_betweenx(time_range_interpolated, orbit1_interpolated, orbit2_interpolated, color=mig_col_cone, alpha=0.3)
 
-    ax1.plot(planet_orbit1, planet_time1, color=mig_col, linestyle='-', lw=lw_mig, zorder=9)
-    ax1.plot(planet_orbit2, planet_time2, color=mig_col, linestyle='-', lw=lw_mig, zorder=9)
+    ax1.plot(planet_orbit1, planet_time1, color=mig_col, linestyle='--', lw=lw_mig, zorder=9)
+    ax1.plot(planet_orbit2, planet_time2, color=mig_col, linestyle='--', lw=lw_mig, zorder=9)
 
-color_mig = qred_dark
-ax1.text(0.44, 0.58, 'Terrestrial planet\nmigration corridor', color=color_mig, rotation=0, ha="center", va="center", fontsize=fsize-3, transform=ax1.transAxes)
-ax1.text(0.38, 0.935, r'$0.1 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-40, va='center', ha='center', transform=ax1.transAxes)
-ax1.text(0.38, 0.777, r'$0.2 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-33, va='center', ha='center', transform=ax1.transAxes)
-ax1.text(0.39, 0.433, r'$0.01 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-46, va='center', ha='center', transform=ax1.transAxes)
-ax1.text(0.38, 0.37, r'$0.1 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-38, va='center', ha='center', transform=ax1.transAxes)
+color_mig = qgray_dark
+ax1.text(0.46, 0.58, 'Terrestrial planet\nmigration corridor', color=color_mig, rotation=0, ha="center", va="center", fontsize=fsize-3, transform=ax1.transAxes)
+ax1.text(0.40, 0.935, r'$0.1 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-40, va='center', ha='center', transform=ax1.transAxes)
+ax1.text(0.40, 0.777, r'$0.2 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-33, va='center', ha='center', transform=ax1.transAxes)
+ax1.text(0.41, 0.423, r'$0.01 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-46, va='center', ha='center', transform=ax1.transAxes)
+ax1.text(0.40, 0.37, r'$0.1 \, M_{\mathrm{Earth}}$', color=color_mig, size=fsize-7, rotation=-38, va='center', ha='center', transform=ax1.transAxes)
 
-color_dot = qred
+color_dot = qgray_dark
 ax1.plot(3.0, 0.35e+6, marker='.', markersize=15, color="k", markerfacecolor=color_dot, alpha=0.99, zorder=20)
 ax1.plot(5.0, 1.0e+6, marker='.', markersize=15, color="k", markerfacecolor=color_dot, alpha=0.99, zorder=20)
 
 lwa=1.5
 
-x_arrows = 1.015
+x_arrows = 0.015
 x_text   = x_arrows-0.01
 ax1.annotate("", xy=(x_arrows, 1.0), xycoords='axes fraction', xytext=(x_arrows, 0.828), textcoords='axes fraction', arrowprops=dict(arrowstyle="-|>,head_length=0.35,head_width=0.2", lw=lwa, connectionstyle="arc3", fc=qgray_light, ec=qgray_light), transform=ax1.transAxes)
-ax1.text(x_text, 0.745, 'Class II stage', color=qgray_dark, size=fsize, rotation=90, va='center', ha='left', fontsize=fsize-5, transform=ax1.transAxes)
+ax1.text(x_text, 0.745, 'Class II disk', color=qgray_light, size=fsize, rotation=90, va='center', ha='left', fontsize=fsize-5, transform=ax1.transAxes)
 ax1.annotate("", xy=(x_arrows, 0.66), xycoords='axes fraction', xytext=(x_arrows, 0.480), textcoords='axes fraction', arrowprops=dict(arrowstyle="|-|,widthA=0.12,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=qgray_light, ec=qgray_light), transform=ax1.transAxes)
 ax1.annotate("", xy=(x_arrows, 0.47), xycoords='axes fraction', xytext=(x_arrows, 0.302), textcoords='axes fraction', arrowprops=dict(arrowstyle="-|>,head_length=0.35,head_width=0.2", lw=lwa, connectionstyle="arc3", fc=qgray_light, ec=qgray_light), transform=ax1.transAxes)
-ax1.text(x_text, 0.236, 'Infall stage', color=qgray_dark, size=fsize, rotation=90, va='center', ha='left', fontsize=fsize-5, transform=ax1.transAxes)
+ax1.text(x_text, 0.236, 'Class I disk', color=qgray_light, size=fsize, rotation=90, va='center', ha='left', fontsize=fsize-5, transform=ax1.transAxes)
 ax1.annotate("", xy=(x_arrows, 0.171), xycoords='axes fraction', xytext=(x_arrows, 0.001), textcoords='axes fraction', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=qgray_light, ec=qgray_light), transform=ax1.transAxes)
 
 # ##### GAP OPENING CONTOURS
@@ -190,7 +191,7 @@ ax1.annotate("", xy=(x_arrows, 0.171), xycoords='axes fraction', xytext=(x_arrow
 
 
 ########## RESERVOIRS
-ax1.text(0.88, 0.17e+6, r'H$_2$O'+'\nsnowline', color="k", size=fsize, rotation=0, va='center', ha='center', fontsize=fsize-3)
+ax1.text(0.88, 0.17e+6, r'Water'+'\nsnow line', color="#530971", size=fsize, rotation=0, va='center', ha='center', fontsize=fsize-3)
 
 resI_txt = ax1.text(3.65, 3.8e+5, 'Reservoir I', color=reds[4], rotation=0, ha="left", fontsize=fsize+4)
 resII_txt = ax1.text(8.2, 2.0e+6, 'Reservoir II', color=blues[6], rotation=0, ha="center", fontsize=fsize+4)
