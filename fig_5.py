d5a58edaad0a8177ab25108411901743f931c2c8
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2020-12-07 13:21:14 +0000

    Fig 5 update

--- fig_5.py
+++ fig_5.py
@@ -111,7 +111,7 @@ ax4.text(0.05, 0.85, 'Aqueous alteration times in meteorites', color=qgray_dark,
 ax2.text(0.01, 0.96, 'B', color="k", rotation=0, ha="left", va="center", fontsize=fsize+3, transform=ax2.transAxes)
 ax2.text(0.05, 0.96, 'Simulation', color=qgray_dark, rotation=0, ha="left", va="center", fontsize=fsize-6, transform=ax2.transAxes, bbox=dict(fc='white', ec="white", alpha=0.8, pad=0.1, boxstyle='round'))
 
-ax4.annotate(r'Carbonaceous reservoir (CC)', xy=(0.30, 0.25), va="center", ha="left", fontsize=fsize-6, color=qblue, transform=ax4.transAxes)
+ax4.annotate(r'Carbonaceous reservoir (CC)', xy=(0.40, 0.25), va="center", ha="left", fontsize=fsize-6, color=qblue, transform=ax4.transAxes)
 ax4.annotate(r'Non-carbonaceous reservoir (NC)', xy=(0.12, 0.09), va="center", ha="left", fontsize=fsize-6, color=qred, transform=ax4.transAxes)
 
 ax2.plot([0], [0], ls="-", c="black", lw=lw, label=r"Hydrous rock, $T_\mathrm{hydr} \leq T_\mathrm{max} < T_\mathrm{decomp}$")
@@ -120,54 +120,7 @@ ax2.plot([0], [0], ls="--", c="black", lw=lw, label=r"Water ice, $T_\mathrm{max}
 legend1 = ax2.legend(fontsize=fsize-4, loc=1, title="Water phase in planetesimals")
 plt.setp(legend1.get_title(), fontsize=fsize-4)
 
-
-############# CORE FORMATION AGES AND HYDROTHERMAL ACTIVITY
-
-# Kruijer+17 NC+CC irons
-
-## Calc error propagated mean age
-import uncertainties
-from uncertainties import ufloat
-
-# NC_irons = np.array([ufloat(0.3, 0.5), 
-#                      ufloat(0.8, 0.5), 
-#                      ufloat(1.2, 0.5), 
-#                      ufloat(1.8, 0.7), 
-#                      ufloat(1.5, 0.6)])#,
-#                      # ufloat(6.0, 0.8)]) # IAB
-
-# CC_irons = np.array([ufloat(2.6, 1.3), 
-#                         ufloat(2.3, 1.2), 
-#                         ufloat(2.3, 0.6), 
-#                         ufloat(2.5, 0.7), 
-#                         ufloat(2.2, 1.1), 
-#                         ufloat(2.8, 0.7)])
-
-# NC_hydr = np.array([ufloat(2.4, 1.8)])
-
-# CC_hydr = np.array( [ufloat(4.2, 0.8), 
-#                         ufloat(5.1, 0.5), 
-#                         ufloat(4.8, 3.0), 
-#                         ufloat(4.9, 0.7), 
-#                         ufloat(4.8, 0.5), 
-#                         ufloat(4.7, 1.3)])
-
-# # Calculate mean and associated error:
-# NC_iron_mean = np.mean(NC_irons)
-# CC_iron_mean = np.mean(CC_irons)
-# NC_hydr_mean = np.mean(NC_hydr)
-# CC_hydr_mean = np.mean(CC_hydr)
-
-# print("NC_iron_mean:", NC_iron_mean)
-# print("CC_iron_mean:", CC_iron_mean)
-# print("NC_hydr_mean:", NC_hydr_mean)
-# print("CC_hydr_mean:", CC_hydr_mean)
-
-# CR mean, Jilly-Rehak 2017
-CR_hydr = np.array( [ufloat(4.66, 3.0), 
-                     ufloat(3.66, 3.0)])
-CR_hydr_mean = np.mean(CR_hydr)
-print("CR_hydr_mean:", CR_hydr_mean)
+############# HYDROTHERMAL ACTIVITY
 
 # ### Annotate ages as error bars
 lwa            = 1.5
@@ -183,43 +136,43 @@ ages_y_base_cc = 0.09
 # CAI offset
 CAI_offset = 0.0
 
-# OC: 2.4 (+1.8/-1.3), 1.1 - 4.2
+# OC: 2.4 (-1.3/+1.8), 1.1 - 4.2
 y_offset = 0.00
 ax4.annotate("", xy=(4.2+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(1.1+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=nc_color_ages, ec=nc_color_ages))
 ax4.plot(2.4+CAI_offset, ages_y_base_cc+y_offset, color=nc_color_ages, marker='D', mec="white", zorder=20)
 ax4.annotate('OC', xy=(1.05, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=nc_color_ages, transform=ax4.transAxes)
 
-# TL: 3.8 (-1.1/+1.3), 2.7 - 5.1
+# TL: 4.1 (-1.1/+1.3), 3.0 - 5.4
 y_offset = 0.05
-ax4.annotate("", xy=(5.1+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(2.7+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
-ax4.plot(3.8+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
-ax4.annotate('Tagish Lake', xy=(2.6, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-9, color=cc_color_ages, transform=ax4.transAxes)
+ax4.annotate("", xy=(5.4+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.0+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
+ax4.plot(4.1+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
+ax4.annotate('Tagish Lake', xy=(2.9, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-9, color=cc_color_ages, transform=ax4.transAxes)
 
-# CM: 3.9 (-0.4/+0.5), 3.5 - 4.4
+# CM: 4.1 (-0.4/+0.5), 3.7 - 4.6
 y_offset = 0.10
-ax4.annotate("", xy=(4.4+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.5+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
-ax4.plot(3.9+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
-ax4.annotate('CM', xy=(3.4, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
+ax4.annotate("", xy=(4.6+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.7+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
+ax4.plot(4.1+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
+ax4.annotate('CM', xy=(3.6, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
 
-# CI: 4.15, 3.5 - 4.8
+# CV: 4.2 (-0.7/+0.8), 3.5 - 5.0
 y_offset = 0.15
-ax4.annotate("", xy=(4.8+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.5+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
-ax4.plot(4.15+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
-ax4.annotate('CI', xy=(3.4, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
-
-# CR: 4.2  ± 2.1, 2.1 - 6.3
-y_offset = 0.20
-ax4.annotate("", xy=(6.0, ages_y_base_cc+y_offset), xycoords='data', xytext=(2.1+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
-ax4.plot(4.2+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
-ax4.annotate('CR', xy=(2.0, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
-
-# CV: 4.2 (+0.8/-0.7), 3.5 - 5.0
-y_offset = 0.25
 ax4.annotate("", xy=(5.0+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.5+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
 ax4.plot(4.2+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
 ax4.annotate('CV', xy=(3.4, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
 
-# CO: 5.1 (+0.5/-0.4), 4.7 - 5.6
+# CR: 4.2 (-1.3/+1.8), 2.9 - 6.3
+y_offset = 0.20
+ax4.annotate("", xy=(6.0, ages_y_base_cc+y_offset), xycoords='data', xytext=(2.9+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
+ax4.plot(4.2+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
+ax4.annotate('CR', xy=(2.8, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
+
+# CI: 4.4 (-0.5/+0.6), 3.9 - 5.0
+y_offset = 0.25
+ax4.annotate("", xy=(5.0+CAI_offset, ages_y_base_cc+y_offset), xycoords='data', xytext=(3.9+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
+ax4.plot(4.4+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
+ax4.annotate('CI', xy=(3.8, ages_y_base_cc+y_offset), va="center", ha="right", fontsize=fsize-8, color=cc_color_ages, transform=ax4.transAxes)
+
+# CO: 5.1 (-0.4/+0.5), 4.7 - 5.6
 y_offset = 0.30
 ax4.annotate("", xy=(5.6, ages_y_base_cc+y_offset), xycoords='data', xytext=(4.7+CAI_offset, ages_y_base_cc+y_offset), textcoords='data', arrowprops=dict(arrowstyle="|-|,widthA=0.0,widthB=0.0", lw=lwa, connectionstyle="arc3", fc=cc_color_ages, ec=cc_color_ages))
 ax4.plot(5.1+CAI_offset, ages_y_base_cc+y_offset, color=cc_color_ages, marker='s', mec="white", zorder=20)
